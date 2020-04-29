be4b8f0dd4137abe31a99d98a69fc07d9abd5aa3
amruthsoppadandi <53913375+amruthsoppadandi@users.noreply.github.com>
2020-04-29 19:55:25 +0530

    Create ProblemStatement.java

---
+++ src/graphs/ProblemStatement.java
@@ -0,0 +1,16 @@
+package graphs;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProblemStatement {
+
+	
+	public static void main(String[] args) {
+		 Period period = Period.of(0, 0, 0);
+         System.out.println(period);
+	}
+
+}
